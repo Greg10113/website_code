20a2920557be2ab0f5125b34d511b599d2588640
Gregory Amberes <greg10113@gmail.com>
2023-11-09 19:38:18 -0500

    Upate Nav Bar, and fix up links

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -17,16 +17,18 @@ export default function Middle() {
             {/* Card 1 */}
             <div className="p-6 bg-gray-100 rounded-lg">
               <div className="mb-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="hi-outline hi-cog inline-block w-12 h-12 text-indigo-500"
-                  viewBox="0 0 576 512"
-                  stroke="currentColor"
-                >
-                  <path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
-                </svg>
+                <Link href={''}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="hi-outline hi-cog inline-block w-12 h-12 text-indigo-500"
+                    viewBox="0 0 576 512"
+                    stroke="currentColor"
+                  >
+                    <path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
+                  </svg>
+                </Link>
               </div>
-              <h3 className="text-lg font-bold mb-2">Zelda Game Info</h3>
+              <h3 className="text-lg font-bold mb-2">Zelda Compendium</h3>
 
               <p className="text-sm leading-6 text-gray-600">
                 This project was built with python and flask. It works by
@@ -107,33 +109,32 @@ export default function Middle() {
               <h3 className="text-lg font-bold mb-2">Pokedex</h3>
 
               <p className="text-sm leading-6 text-gray-600">
-                Metus potenti velit sollicitudin porttitor magnis elit lacinia
-                tempor varius, ut cras orci vitae parturient id nisi vulputate
-                consectetur, primis venenatis cursus tristique malesuada viverra
-                congue risus.
+                This project is another simple one where I created a page that
+                fetches Pokémon data from the PokeApi and displays it for the
+                client.
               </p>
             </div>
 
             {/* Card 4 */}
             <div className="p-6 bg-gray-100 rounded-lg">
               <div className="mb-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="hi-outline hi-cog inline-block w-12 h-12 text-indigo-500"
-                  viewBox="0 0 640 512"
-                  stroke="currentColor"
-                >
-                  <path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z" />
-                </svg>
+                <Link href={'https://github.com/Greg10113'}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="hi-outline hi-cog inline-block w-12 h-12 text-indigo-500"
+                    viewBox="0 0 640 512"
+                    stroke="currentColor"
+                  >
+                    <path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z" />
+                  </svg>
+                </Link>
               </div>
 
               <h3 className="text-lg font-bold mb-2">GitHub</h3>
 
               <p className="text-sm leading-6 text-gray-600">
-                Metus potenti velit sollicitudin porttitor magnis elit lacinia
-                tempor varius, ut cras orci vitae parturient id nisi vulputate
-                consectetur, primis venenatis cursus tristique malesuada viverra
-                congue risus.
+                Here is my GitHub, where you can take a look at some other small
+                projects I have worked on.
               </p>
             </div>
           </div>
@@ -149,8 +150,7 @@ export default function Middle() {
         <p>
           As a developer, I am most knowledgeable in web development and its
           associated technologies, including HTML, CSS, JavaScript, React,
-          Next.js, SQL, Node.js, and Express. I am also working to include
-          Python in this stack.
+          Next.js, SQL, Node.js, Express, and Python.
         </p>
       </div>
     </div>
